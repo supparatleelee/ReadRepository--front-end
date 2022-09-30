0eaa69902a149b872bab6f3b282d86acdbb1cd54
supparatleelee <supparat.lst@gmail.com>
2022-09-30 15:31:54 +0700

    prepared flex style for .map item lists later in search result page

--- src/features/search/SearchResultContainer.js
+++ src/features/search/SearchResultContainer.js
@@ -2,18 +2,14 @@ import SearchResultItem from './SearchResultItem';
 
 function SearchResultContainer() {
   return (
-    <div className="h-80 mt-2 border-radius-10px">
+    <div className="h-80 border-radius-10px">
       {/* .map here */}
       {/* {bookList.map((item) => (
         <SearchResultItem />
       ))} */}
-      <div className="d-flex justify-content-center">
+      <div className="d-flex flex-wrap">
         <SearchResultItem />
         <SearchResultItem />
-        <SearchResultItem />
-        <SearchResultItem />
-      </div>
-      <div className="d-flex justify-content-center mt-4">
         <SearchResultItem />
         <SearchResultItem />
         <SearchResultItem />
--- src/features/search/SearchResultItem.js
+++ src/features/search/SearchResultItem.js
@@ -5,7 +5,7 @@ import AddToCollectionDropdown from './AddToCollectionDropdown';
 function SearchResultItem() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div style={{ width: '200px' }}>
+    <div style={{ width: '200px', marginTop: '20px' }}>
       <img
         src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B600%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&source=url%5Bhttps://prodimage.images-bn.com/pimages/9780544947221_p0_v6_s600x595.jpg%5D&scale=options%5Blimit%5D,size%5B600x10000%5D&sink=format%5Bwebp%5D"
         width="80px"
